1a2686fd1a5bf78822929b1073a3de8bb8585df4
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-04-01 14:41:11 +0900

    feat: Complete fizzbuzz
    15배수일 때 fizzbuzz, 3의 배수일 때 fizz, 5의 배수일 때 buzz, 나머지는
    숫자로 나타냈습니다

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,19 @@
+function fizzbuzz {
+    for(let i = 1; i < 20; i++) {
+        if (i%15==0) {
+            alert('fizzbuzz');
+	} else if (i%3==0) {
+            alert('fizz');
+	} else if (i%5==0) {
+            alert('buzz');
+	} else {
+            alert(i)
+	}
+    }
+}
 
+function init() {
+    fizzbuzz();
+}
+
+init();
